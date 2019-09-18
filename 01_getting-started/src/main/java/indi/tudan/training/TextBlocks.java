b81351de8997de3a3da9d9a37e1b79f22775076a
tudan110 <wtudan@126.com>
2019-09-18 14:31:20 +0800

    Text Blocks

---
+++ 01_getting-started/src/main/java/indi/tudan/training/TextBlocks.java
@@ -0,0 +1,26 @@
+package indi.tudan.training;
+
+/**
+ * Java 13 预览功能：文本块
+ * <p>
+ * 您需要下载 JDK 13。在 IntelliJ 中，转到 File → Project Structure 并确保在 Project SDK 下选择了 JDK 13。
+ * 要将文本块作为预览功能启用，请务必选择 13 (Preview) - Switch expressions, text blocks
+ *
+ * @author wangtan
+ * @date 2019-09-18 14:19:17
+ * @since 1.0
+ */
+public class TextBlocks {
+
+    public static void main(String[] args) {
+
+        String html = """
+              <html>
+                  <body>
+                      <p>Hello, world</p>
+                  </body>
+              </html>
+              """;
+        System.out.println(html);
+    }
+}
