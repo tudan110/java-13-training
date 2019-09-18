a1afbe89bf08d60ca151b00285b1200aebdf1956
tudan110 <wtudan@126.com>
2019-09-18 14:31:07 +0800

    Hello World!

---
+++ 01_getting-started/src/main/java/indi/tudan/training/HelloWorld.java
@@ -0,0 +1,15 @@
+package indi.tudan.training;
+
+/**
+ * @author wangtan
+ * @date 2019-09-18 14:18:36
+ * @since 1.0
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        var s = "Hello world!";
+        System.out.println(s);
+    }
+}
